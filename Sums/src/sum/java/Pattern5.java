afb88015ad78a2eb40478982133e6eccd53c6dc0
naveenopm <naveenkumaropm@gmail.com>
2017-08-19 13:56:28 +0530

    Printing a 4321in decreasing order program using while loop

---
+++ Sums/src/sum/java/Pattern5.java
@@ -0,0 +1,20 @@
+package sum.java;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		int r=1;
+		do {
+			int b=5;
+			while(b>r)
+			{   
+				b--;
+				System.out.print(b);
+			}
+			r++;
+			System.out.println();
+		}while(r<5);
+
+	}
+
+}
